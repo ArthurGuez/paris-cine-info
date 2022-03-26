97a7aa32adda07d879e94fe5c391e6ca849290b9
Arthur Guez <arthurguez29@gmail.com>
2022-03-26 17:41:23 +0100

    feat(constants): add settings variables

---
+++ constants/settings.ts
@@ -0,0 +1,4 @@
+export const LAYOUTS = { grid: 'Affiches', list: 'Liste' };
+export const RATINGS = ['IMDB', 'senscritique', 'allociné presse', 'allociné spectateurs'];
+export const THEMES = { dark: 'Sombre', light: 'Clair' };
+export const TITLES = { french: 'Français', originals: 'Originaux' };

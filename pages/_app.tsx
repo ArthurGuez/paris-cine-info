fc2431b16c0a9bc8e683f8f15ff3437974a7cb0a
Arthur GUEZ <aguez@lesechos.fr>
2024-01-23 17:07:45 +0100

    chore(next): change project structure to move to app router

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from 'next/app';
-
-import '../globals.sass';
-
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
-}
-
-export default MyApp;

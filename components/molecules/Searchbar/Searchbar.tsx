b6e4b601baf84be554a211e2fc33f9fe5863a2a0
Arthur Guez <arthurguez29@gmail.com>
2025-02-24 23:05:22 +0100

    chore(next): change project structure to move to app router

--- components/molecules/Searchbar/Searchbar.tsx
+++ components/molecules/Searchbar/Searchbar.tsx
@@ -2,18 +2,19 @@ import MagnifyingGlass from '../../../icons/magnifying-glass.svg';
 import styles from './Searchbar.module.sass';
 
 export default function Searchbar() {
-	return (
-		<form className={styles.searchbar}>
-			<input className={styles.searchbar__input} type="text" />
-			<label htmlFor="submit">
-				<input type="submit" id="submit" style={{ display: 'none' }} />
-				<MagnifyingGlass
-					className={styles.searchbar__submit}
-					fill="var(--primary-color)"
-					height="20px"
-					width="20px"
-				/>
-			</label>
-		</form>
-	);
+  return (
+    <form className={styles.searchbar}>
+      <input className={styles.searchbar__input} type='text' />
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label htmlFor='submit'>
+        <input type='submit' id='submit' style={{ display: 'none' }} />
+        <MagnifyingGlass
+          className={styles.searchbar__submit}
+          fill='var(--primary-color)'
+          height='20px'
+          width='20px'
+        />
+      </label>
+    </form>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ['tsx'],
-	reactStrictMode: true,
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg$/,
-			use: ['@svgr/webpack'],
-		});
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
 
-		return config;
-	},
+    return config;
+  },
 };
 
 module.exports = nextConfig;

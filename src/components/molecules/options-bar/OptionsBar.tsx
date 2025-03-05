ffb2771391268a045cce0a75564e164c839d42d1
Arthur Guez <arthurguez29@gmail.com>
2025-03-05 12:06:19 +0100

    refactor(svg): change markup and add dynamic color

--- src/components/molecules/options-bar/OptionsBar.tsx
+++ src/components/molecules/options-bar/OptionsBar.tsx
@@ -6,10 +6,10 @@ export default function OptionsBar() {
   return (
     <div className="flex items-center uppercase">
       <OptionsBarItem label="Filtres">
-        <Filters color="#e53246" height="30px" width="30px" />
+        <Filters color="var(--accent)" height="30px" width="30px" />
       </OptionsBarItem>
       <OptionsBarItem label="Réglages">
-        <Gear color="#e53246" height="25px" width="25px" />
+        <Gear color="var(--accent)" height="25px" width="25px" />
       </OptionsBarItem>
     </div>
   );

f2502ccfd7f46e51d248b49dc74558e88a9ab736
Arthur Guez <arthurguez29@gmail.com>
2025-03-13 18:20:39 +0100

    feat(search): add magnifying glass icon

--- src/components/molecules/search-bar/SearchBar.tsx
+++ src/components/molecules/search-bar/SearchBar.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Component as MagnifyingGlass } from '../../../icons/magnifying-glass.svg?svgUse';
 
 const SEARCH_BAR_PLACEHOLDER = 'Rechercher un film, un réalisateur, une salle...';
 
@@ -7,14 +8,21 @@ export default function SearchBar() {
 
   return (
     <form className="flex flex-1 items-center">
-      <input
-        className="h-10 w-full rounded-full border border-accent bg-transparent px-5 text-body outline-none"
-        name="query"
-        onChange={(event) => setSearchTerm(event.target.value)}
-        placeholder={SEARCH_BAR_PLACEHOLDER}
-        value={searchTerm}
-      />
-      <input className="hidden cursor-pointer" type="submit" />
+      <div className="relative w-full">
+        <input
+          className="h-10 w-full rounded-full border border-accent bg-transparent px-5 text-body outline-none"
+          name="query"
+          onChange={(event) => setSearchTerm(event.target.value)}
+          placeholder={SEARCH_BAR_PLACEHOLDER}
+          value={searchTerm}
+        />
+        <MagnifyingGlass
+          height="25px"
+          width="25px"
+          color="var(--accent)"
+          className="absolute top-1/2 right-5 -translate-y-1/2"
+        />
+      </div>
     </form>
   );
 }

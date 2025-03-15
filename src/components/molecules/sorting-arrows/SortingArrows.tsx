240c35f5a1f054760dfec3bbe752451385f67a40
Arthur Guez <arthurguez29@gmail.com>
2025-03-15 14:32:51 +0100

    refactor(sorting): change arrows

--- src/components/molecules/sorting-arrows/SortingArrows.tsx
+++ src/components/molecules/sorting-arrows/SortingArrows.tsx
@@ -3,8 +3,12 @@ import { Component as Triangle } from '../../../icons/triangle.svg?svgUse';
 export default function SortingArrows() {
   return (
     <div className="flex flex-col">
-      <Triangle color="var(--accent)" height="21px" width="21px" />
-      <Triangle className="rotate-180" color="var(--accent)" height="21px" width="21px" />
+      <div>
+        <Triangle color="var(--accent)" height="20px" width="20px" />
+      </div>
+      <div className="rotate-180">
+        <Triangle color="var(--accent)" height="20px" width="20px" />
+      </div>
     </div>
   );
 }

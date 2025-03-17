9a5725a79a1ed2bb697e7135cab27748884ef12c
Arthur Guez <arthurguez29@gmail.com>
2025-03-17 14:37:38 +0100

    refactor(rating): remove star icon when valus is 0

--- src/components/atoms/cells/Rating.tsx
+++ src/components/atoms/cells/Rating.tsx
@@ -6,8 +6,14 @@ interface Props {
 export default function Rating({ value }: Props) {
   return (
     <div className="flex w-15 justify-center gap-1 rounded-md border border-accent px-2 py-1">
-      {value === '0' ? '-' : value}
-      <img alt="" src={star} />
+      {value === '0' ? (
+        '-'
+      ) : (
+        <>
+          {value}
+          <img alt="" src={star} />
+        </>
+      )}
     </div>
   );
 }

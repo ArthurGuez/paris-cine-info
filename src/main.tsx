e32a6e18ec195c1c733d8ca3dde35604ed71d3a3
Arthur Guez <arthurguez29@gmail.com>
2025-03-19 15:56:08 +0100

    refactor(react): add strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
+import { StrictMode } from 'react';
 
 // Set up a Router instance
-const router = createRouter({
-  routeTree,
-  defaultPreload: 'intent',
-});
+const router = createRouter({ routeTree, defaultPreload: 'intent' });
 
 // Register things for typesafety
 declare module '@tanstack/react-router' {
@@ -19,5 +17,9 @@ const rootElement = document.getElementById('app')!;
 
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
-  root.render(<RouterProvider router={router} />);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />{' '}
+    </StrictMode>,
+  );
 }

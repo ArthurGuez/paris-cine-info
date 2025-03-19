7a7eb0f8f19e0655edf891cc72a014b0e0c9c3cf
Arthur Guez <arthurguez29@gmail.com>
2025-03-19 17:23:34 +0100

    refactor(rating): change size and vertical alignment

--- src/components/atoms/cells/Rating.tsx
+++ src/components/atoms/cells/Rating.tsx
@@ -6,13 +6,13 @@ interface Props {
 
 export default function Rating({ value }: Props) {
   return (
-    <div className="flex w-15 justify-center gap-1 rounded-md border border-accent px-2 py-1">
+    <div className="flex w-15 items-center justify-center gap-1 rounded-md border border-accent px-2 py-1">
       {value === '0' ? (
         '-'
       ) : (
         <>
           {value}
-          <Star color="var(--accent)" />
+          <Star color="var(--accent)" width="15" height="15" />
         </>
       )}
     </div>

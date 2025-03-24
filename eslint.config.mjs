7b47b44b59c9a8da9d1400b00c17f13882ca79ad
Arthur Guez <arthurguez29@gmail.com>
2025-03-24 12:37:56 +0100

    feat(eslint): add globals, react plugin and react-hooks plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,14 +2,21 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import reactPlugin from 'eslint-plugin-react';
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
+  reactHooks.configs['recommended-latest'],
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
   { ignores: ['**/dist/**', 'lint-staged.config.js'] },
   {
     languageOptions: {
+      globals: globals.browser,
       parserOptions: { projectService: true, tsconfigRootDir: import.meta.dirname },
     },
   },

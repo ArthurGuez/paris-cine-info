ea2b4cb8b11b86fa66d549bdb7cf18462f4046ff
Arthur Guez <arthurguez29@gmail.com>
2025-03-27 10:18:33 +0100

    feat(services): add movie and getAllMoviesResponse schemas & infer types from them

---
+++ src/services/schemas.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod';
+
+export const movieSchema = z.object({
+  ac: z.string(),
+  ap_r: z.string(),
+  as_r: z.string(),
+  co: z.string(),
+  di: z.string(),
+  du: z.string(),
+  ge: z.string(),
+  id: z.string(),
+  i_id: z.string(),
+  im_r: z.string(),
+  newstatus: z.string(),
+  o_ti: z.string(),
+  rel: z.string(),
+  ret: z.string(),
+  sc_r: z.string(),
+  sc_u: z.string(),
+  ti: z.string(),
+  ye: z.string(),
+});
+
+export const getAllMoviesResponseSchema = z.object({
+  data: z.array(movieSchema),
+  favs: z.boolean(),
+  follow_movs: z.array(z.string()),
+});
--- src/services/types.ts
+++ src/services/types.ts
@@ -1,26 +1,5 @@
-export interface Movie {
-  ac: string;
-  ap_r: string;
-  as_r: string;
-  co: string;
-  di: string;
-  du: string;
-  ge: string;
-  id: string;
-  i_id: string;
-  im_r: string;
-  newstatus: string;
-  o_ti: string;
-  rel: string;
-  ret: string;
-  sc_r: string;
-  sc_u: string;
-  ti: string;
-  ye: string;
-}
+import { z } from 'zod';
+import { getAllMoviesResponseSchema, movieSchema } from './schemas';
 
-export interface GetAllMoviesResponse {
-  data: Movie[];
-  favs: boolean;
-  follow_movs: string[];
-}
+export type Movie = z.infer<typeof movieSchema>;
+export type GetAllMoviesResponse = z.infer<typeof getAllMoviesResponseSchema>;

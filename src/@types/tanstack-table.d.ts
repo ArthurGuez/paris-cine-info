3dd129cd5dd4789d0233c083a87e8767e7547431
ArthurGuez <arthurguez29@gmail.com>
2026-08-25 11:11:17 +0200

    refactor(types): remove dead SortingFns augmentation for tanstack table

--- src/@types/tanstack-table.d.ts
+++ src/@types/tanstack-table.d.ts
@@ -1,11 +1,7 @@
 import '@tanstack/react-table';
-import type { SortingFn } from '@tanstack/react-table';
 
 declare module '@tanstack/react-table' {
   interface ColumnMeta {
     className?: string;
-  }
-  interface SortingFns {
-    sortingBookmarks: SortingFn<unknown>;
   }
 }

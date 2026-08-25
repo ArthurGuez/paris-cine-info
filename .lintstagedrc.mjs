f1878d1883200310b279317312a156dde145222b
ArthurGuez <arthurguez29@gmail.com>
2026-08-25 12:07:50 +0200

    chore(config): add lint-staged rules

--- .lintstagedrc.mjs
+++
@@ -1,6 +0,0 @@
-export default {
-  // Run type-check on changes to TypeScript files
-  '**/*.ts?(x)': () => 'pnpm run typecheck',
-  // Run Oxlint on changes to JavaScript/TypeScript files
-  '**/*.(ts|js)?(x)': (filenames) => `pnpm run lint . ${filenames.join(' ')}`,
-};
